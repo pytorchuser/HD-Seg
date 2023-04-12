4baf771cdcc93315e53b111ced19d7b76b0ccf76
huajiang <huajuan1hao@163.com>
2023-04-12 23:12:09 +0800

    [Fix]schedule配置文件成1.x版本

--- configs/_base_/schedules/schedule_800.py
+++ configs/_base_/schedules/schedule_800.py
@@ -1,11 +1,29 @@
 # optimizer
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
-optimizer_config = dict()
+optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
 # learning policy
-lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
-# runtime settings
-# training by iters
-runner = dict(type='IterBasedRunner', max_iters=800)
-checkpoint_config = dict(by_epoch=False, interval=400)
-evaluation = dict(interval=400, metric='mIoU', pre_eval=True)
-
+param_scheduler = [
+    dict(
+        type='PolyLR',
+        eta_min=1e-4,
+        power=0.9,
+        begin=0,
+        end=800,
+        by_epoch=False)
+]
+# training schedule for 240k
+train_cfg = dict(
+    type='IterBasedTrainLoop', max_iters=800, val_interval=80)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(type='CheckpointHook',
+                    by_epoch=False,
+                    interval=400,
+                    metric='mIoU',
+                    pre_eval=True),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='SegVisualizationHook'))
--- configs/_base_/schedules/schedule_epoch.py
+++ configs/_base_/schedules/schedule_epoch.py
@@ -1,11 +1,32 @@
 # optimizer
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
-optimizer_config = dict()
+optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
 # learning policy
-lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=True)
-# runtime settings
-# training by epochs
-runner = dict(type='EpochBasedRunner', max_epochs=500)
-checkpoint_config = dict(by_epoch=True, interval=5, max_keep_ckpts=1)
-# save_best = ['acc', 'top', 'AR@', 'auc', 'precision', 'mAP', 'mDice', 'mIoU', 'mAcc', 'aAcc']
-evaluation = dict(interval=5, metric=['mDice', 'mIoU'], pre_eval=True, save_best='mDice')
+param_scheduler = [
+    dict(
+        type='PolyLR',
+        eta_min=1e-4,
+        power=0.9,
+        begin=0,
+        end=240000,
+        by_epoch=False)
+]
+# training schedule by epoch
+train_cfg = dict(
+    type='EpochBasedTrainLoop', max_epochs=50, val_begin=10, val_interval=2)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    # save_best = ['acc', 'top', 'AR@', 'auc', 'precision', 'mAP', 'mDice', 'mIoU', 'mAcc', 'aAcc']
+    checkpoint=dict(type='CheckpointHook',
+                    by_epoch=True,
+                    interval=5,
+                    max_keep_ckpts=1,
+                    metric=['mDice', 'mIoU'],
+                    save_best=['mIoU'],
+                    rule='greater'),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='SegVisualizationHook'))
