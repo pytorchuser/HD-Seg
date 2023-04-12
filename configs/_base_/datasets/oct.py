ce9bbf3b77d57f22907872a8a9f8599b191f8608
huajiang <huajuan1hao@163.com>
2023-04-12 18:22:39 +0800

    [docs]transforms常用形变添加注释

--- configs/_base_/datasets/oct.py
+++ configs/_base_/datasets/oct.py
@@ -4,16 +4,17 @@ data_root = '../data/OCT/new1'
 img_norm_cfg = dict(
     # mean均值， std标准差，三通道顺序为rgb，以下为pytorch的基模型，如果要从头训练一个数据集，可以自行计算
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+# 不对应(h,w)属性，指最长边和最短边。
 img_scale = (605, 700)
 crop_size = (512, 512)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations',  reduce_zero_label=False),
     # 变化图像和其注释大小的数据增广的流程,图像的最大规模,缩放图像的比例范围
-    dict(type='Resize', img_scale=img_scale, ratio_range=(0.5, 2.0)),
-    # 实现中心裁剪
+    dict(type='Resize', img_scale=img_scale, ratio_range=(0.5, 2.0), keep_ratio=True),
+    # 实现随机裁剪，随机裁剪图像生成 patch 的大小，单个类别可以填充的最大区域的比例
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
-    # 0.5的几率水平反转
+    # 0.5的几率水平翻转
     dict(type='RandomFlip', prob=0.5),
     # 光学上使用一些方法扭曲当前图像和其注释，默认每个转换都有0.5的概率进行
     # 这些转换流程包括：改变亮度，对比形变，将颜色从BGR转换为HSV，改变饱和度，改变色调，将颜色从HSV转换为BGR
