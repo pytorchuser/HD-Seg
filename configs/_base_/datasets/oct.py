98621ff61d55206507855f41546534a8dd8ab6a8
huajiang <huajuan1hao@163.com>
2023-04-13 20:07:34 +0800

    [Fix]数据集配置改为1.x版本

--- configs/_base_/datasets/oct.py
+++ configs/_base_/datasets/oct.py
@@ -1,71 +1,120 @@
 # dataset settings
+# 数据集类型，这将被用来定义数据集
 dataset_type = 'OCTDataset'
-data_root = '../data/OCT/new1'
-img_norm_cfg = dict(
-    # mean均值， std标准差，三通道顺序为rgb，以下为pytorch的基模型，如果要从头训练一个数据集，可以自行计算
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-# 不对应(h,w)属性，指最长边和最短边。
-img_scale = (605, 700)
+# 数据的根路径
+data_root = 'data/OCT/new1'
+# 训练时的裁剪大小
 crop_size = (512, 512)
+# 数据增广时，图像裁剪的大小
+img_scale = (605, 700)
+# 训练流程
 train_pipeline = [
+    # 从文件路径里加载图像
     dict(type='LoadImageFromFile'),
+    # 对于当前图像，加载它的标注图像
     dict(type='LoadAnnotations',  reduce_zero_label=False),
-    # 变化图像和其注释大小的数据增广的流程,图像的最大规模,缩放图像的比例范围
-    dict(type='Resize', img_scale=img_scale, ratio_range=(0.5, 2.0), keep_ratio=True),
-    # 实现随机裁剪，随机裁剪图像生成 patch 的大小，单个类别可以填充的最大区域的比例
-    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    # 调整输入图像大小(resize)和其标注图像的数据增广流程
+    dict(
+        type='RandomResize',
+        # 图像裁剪的大小
+        scale=img_scale,
+        # 数据增广的比例范围
+        ratio_range=(0.5, 2.0),
+        # 调整图像大小时是否保持纵横比
+        keep_ratio=True),
+    # 随机裁剪当前图像和其标注图像的数据增广流程
+    dict(
+        type='RandomCrop',
+        # 随机裁剪的大小
+        crop_size=crop_size,
+        # 单个类别可以填充的最大区域的比
+        cat_max_ratio=0.75),
     # 0.5的几率水平翻转
     dict(type='RandomFlip', prob=0.5),
     # 光学上使用一些方法扭曲当前图像和其注释，默认每个转换都有0.5的概率进行
     # 这些转换流程包括：改变亮度，对比形变，将颜色从BGR转换为HSV，改变饱和度，改变色调，将颜色从HSV转换为BGR
     dict(type='PhotoMetricDistortion'),
-    # 归一化
-    dict(type='Normalize', **img_norm_cfg),
-    # 填充当前图像到指定大小的数据，填充的图像大小，图像的填充值，'gt_semantic_seg'的填充值
-    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
-    # 流程里收集数据的默认格式捆，将 'img' 和 'gt_semantic_seg'装入DataContainer
-    dict(type='DefaultFormatBundle'),
-    # 决定数据里哪些键被传递到分割器里的流程
-    dict(type='Collect', keys=['img', 'gt_semantic_seg'])
+    # 打包用于语义分割的输入数据
+    dict(type='PackSegInputs')
 ]
-# 创建验证数据集时，使用test_pipeline作为compose参数
+# 测试时单尺度数据增强
 test_pipeline = [
+    # 从文件路径里加载图像
     dict(type='LoadImageFromFile'),
+    # 使用调整图像大小(resize)增强
+    dict(type='Resize', scale=img_scale, keep_ratio=True),
+    # 在' Resize '之后添加标注图像，不需要做调整图像大小(resize)的数据变换
+    # 加载数据集提供的语义分割标注
+    dict(type='LoadAnnotations', reduce_zero_label=False),
+    # 打包用于语义分割的输入数据
+    dict(type='PackSegInputs')
+]
+# 图像缩放比例
+img_ratios = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75]
+# 测试时多尺度数据增强
+tta_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=None),
     dict(
-        type='MultiScaleFlipAug',
-        img_scale=img_scale,
-        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0],
-        flip=False,
+        type='TestTimeAug',
         transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img'])
+            [
+                # 多尺度调整图像大小(resize)增强
+                dict(type='Resize', scale_factor=r, keep_ratio=True)
+                for r in img_ratios
+            ],
+            [
+                # 多尺度调整图像翻转增强
+                dict(type='RandomFlip', prob=0., direction='horizontal'),
+                dict(type='RandomFlip', prob=1., direction='horizontal')
+            ],
+            # 加载数据集提供的语义分割标注
+            [dict(type='LoadAnnotations')],
+            # 打包用于语义分割的输入数据
+            [dict(type='PackSegInputs')]
         ])
 ]
-
-data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=4,
-    train=dict(
-        type='RepeatDataset',
-        times=40000,
-        dataset=dict(
-            type=dataset_type,
-            data_root=data_root,
-            img_dir='images/training',
-            ann_dir='annotations/training',
-            pipeline=train_pipeline)),
-    val=dict(
+# 训练数据加载器(dataloader)的配置
+train_dataloader = dict(
+    # 每一个GPU的batch size大小
+    batch_size=2,
+    # 为每一个GPU预读取数据的进程个数
+    num_workers=4,
+    # 在一个epoch结束后关闭worker进程，可以加快训练速度
+    persistent_workers=True,
+    sampler=dict(
+        type='InfiniteSampler',
+        # 训练时进行随机洗牌(shuffle)
+        shuffle=True),
+    # 训练数据集配置
+    dataset=dict(
+        # 数据集类型，详见mmseg/datasets/
+        type=dataset_type,
+        # 数据集的根目录
+        data_root=data_root,
+        # 训练数据的前缀
+        data_prefix=dict(
+            # 原图目录
+            img_path='images/training',
+            # 分割图目录
+            seg_map_path='annotations/training'),
+        # 数据处理流程，它通过之前创建的train_pipeline传递
+        pipeline=train_pipeline))
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(
+        type='DefaultSampler',
+        # 训练时不进行随机洗牌(shuffle)
+        shuffle=False),
+    dataset=dict(
         type=dataset_type,
         data_root=data_root,
-        img_dir='images/validation',
-        ann_dir='annotations/validation',
-        pipeline=test_pipeline),
-    test=dict(
-        type=dataset_type,
-        data_root=data_root,
-        img_dir='images/validation',
-        ann_dir='annotations/validation',
+        data_prefix=dict(
+            img_path='images/validation',
+            seg_map_path='annotations/validation'),
         pipeline=test_pipeline))
+test_dataloader = val_dataloader
+# 精度评估方法，我们在这里使用 IoUMetric 进行评估
+val_evaluator = dict(type='IoUMetric', iou_metrics=['mIoU'])
+test_evaluator = val_evaluator
